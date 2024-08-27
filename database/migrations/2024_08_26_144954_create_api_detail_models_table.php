839706ab5259e768992e655fa6f753db3937ff51
devfajar33 <devfajar33@gmail.com>
2024-08-27 09:16:39 +0700

    First commit

---
+++ database/migrations/2024_08_26_144954_create_api_detail_models_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('api_detail_models', function (Blueprint $table) {
+            $table->string('id', 30)->primary();
+            $table->string('parent_id', 30);
+            $table->string('name', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('api_detail_models');
+    }
+};
